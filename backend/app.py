2646585ee480d0b8510ec0e45d408c4f4119d19e
WN <wnwillyndirangu@gmail.com>
2023-01-20 15:32:58 +0300

    Add comments to the ner endpoint

--- backend/app.py
+++ backend/app.py
@@ -14,23 +14,32 @@ app=create_app()
 def hello_world():
     return "<p>Hello, World!</p>"
 
+# Named entity recognition endpoint 
 @app.route('/ner', methods=['POST'])
 def named_entity_recognition():
     body = request.get_json()
 
+    # get the statement from user
     statement = body.get("statement", None)
-    #prepare the questions
-
-    # infer the questions if same from the siamese Model
+   
+    # generate the vocabulary from the words and tags data
     vocab,tags=get_vocab("./models/ner/words.txt","./models/ner/tags.txt")
+    #initialize the model
     model=initialize_model(tags,"./models/ner/model.pkl.gz")
+
     sentence=statement
+
+    #Predict named entities given the sentence
     preds=predict(sentence, model, vocab, tags)
+    
+    #Package the predicitions with original word in a dictionary i.e. for returning a json object
     predictions={}
     for index,pred in enumerate(preds):
         predictions[sentence.split(' ')[index]]=pred
     
-
-    return jsonify({"data":predictions,
-    'success': True})
+    # Return the serialized json with data(original word with prediction) and success message
+    return jsonify({
+        "data":predictions,
+        'success': True
+        })
     
