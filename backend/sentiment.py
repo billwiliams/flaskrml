4f6d97e4b6dcf6cdec64d0d76dfd1f8fe1c17ac7
WN <wnwillyndirangu@gmail.com>
2023-01-23 15:06:06 +0300

    Add NLTK and process tweet to tokenize an input

--- backend/sentiment.py
+++ backend/sentiment.py
@@ -2,7 +2,24 @@ import trax
 import trax.fastmath.numpy as np
 from trax import layers as tl
 from trax import fastmath
+import re
+import string
+from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords, twitter_samples 
 
+tweet_tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+
+# Stop words are messy and not that compelling; 
+# "very" and "not" are considered stop words, but they are obviously expressing sentiment
+
+# The porter stemmer lemmatizes "was" to "wa".  Seriously???
+
+# I'm not sure we want to get into stop words
+stopwords_english = stopwords.words('english')
+
+# Also have my doubts about stemming...
+from nltk.stem import PorterStemmer
+stemmer = PorterStemmer()
 def classifier(vocab_size=9088, embedding_dim=256, output_dim=2, mode='train'):
     
 
@@ -34,10 +51,44 @@ def classifier(vocab_size=9088, embedding_dim=256, output_dim=2, mode='train'):
     
     # return the model of type
     return model 
+
+  
+def process_tweet(tweet):
+    '''
+    Input: 
+        tweet: a string containing a tweet
+    Output:
+        tweets_clean: a list of words containing the processed tweet
+    
+    '''
+    # remove stock market tickers like $GE
+    tweet = re.sub(r'\$\w*', '', tweet)
+    # remove old style retweet text "RT"
+    tweet = re.sub(r'^RT[\s]+', '', tweet)
+    # remove hyperlinks
+    tweet = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet)
+    # remove hashtags
+    # only removing the hash # sign from the word
+    tweet = re.sub(r'#', '', tweet)
+    # tokenize tweets
+    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+    tweet_tokens = tokenizer.tokenize(tweet)
+    
+    tweets_clean = []
+    for word in tweet_tokens:
+        if (word not in stopwords_english and # remove stopwords
+            word not in string.punctuation): # remove punctuation
+            #tweets_clean.append(word)
+            stem_word = stemmer.stem(word) # stemming word
+            tweets_clean.append(stem_word)
+    
+    return tweets_clean
 model=classifier()
-model.init(trax.shapes.ShapeDtype((1, 1), dtype=np.int32))
+# model.init(trax.shapes.ShapeDtype((1, 1), dtype=np.int32))
 model.init_from_file("./models/sentiment/model.pkl.gz", weights_only=True)
-print(model('[1,0,0,1]'))
+# print(model([1,0,0,1]))
+
+print(process_tweet('AM happy asf'))
 
 
 
