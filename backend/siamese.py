1f7674246350b115e7d615628b3b6bb15a745e29
WN <wnwillyndirangu@gmail.com>
2023-01-27 12:40:44 +0300

    Add a predict function

--- backend/siamese.py
+++ backend/siamese.py
@@ -31,4 +31,53 @@ def Siamese(vocab_size=41699, d_model=128, mode='train'):
     model = tl.Parallel(q_processor, q_processor)
     return model
 
+def predict(question1, question2, threshold, model, vocab, data_generator=data_generator, verbose=False):
+    """Function for predicting if two questions are duplicates.
+    Args:
+        question1 (str): First question.
+        question2 (str): Second question.
+        threshold (float): Desired threshold.
+        model (trax.layers.combinators.Parallel): The Siamese model.
+        vocab (collections.defaultdict): The vocabulary used.
+        data_generator (function): Data generator function. Defaults to data_generator.
+        verbose (bool, optional): If the results should be printed out. Defaults to False.
+    Returns:
+        bool: True if the questions are duplicates, False otherwise.
+    """
+    
+    # use `nltk` word tokenize function to tokenize
+    q1 = nltk.word_tokenize(question1)  # tokenize
+    q2 = nltk.word_tokenize(question2)  # tokenize
+    Q1, Q2 = [], [] # @KEEPTHIS
+    for word in q1:  # encode q1
+        # increment by checking the 'word' index in `vocab`
+        Q1 += [vocab[word]]
+        # GRADING COMMENT: Also valid to use
+        # Q1.extend([vocab[word]])
+    for word in q2:  # encode q2
+        # increment by checking the 'word' index in `vocab`
+        Q2 +=  [vocab[word]]
+        # GRADING COMMENT: Also valid to use
+        # Q2.extend([vocab[word]])
+    
+    # Q1 and Q2 need to be nested inside another list
+    # Call the data generator (built in Ex 01) using next()
+    # pass [Q1] & [Q2] as Q1 & Q2 arguments of the data generator. Set batch size as 1
+    
+    Q1, Q2 = next(data_generator([Q1], [Q2], 1, pad=vocab['<PAD>'], shuffle=False))
+    # Call the model
+    v1, v2 = model((Q1,Q2))
+    # take dot product to compute cos similarity of each pair of entries, v1, v2
+    # don't forget to transpose the second argument
+    d = fastnp.dot(v1,v2.T)
+    # is d greater than the threshold?
+    res = d> threshold
+    
+    if(verbose):
+        print("Q1  = ", Q1, "\nQ2  = ", Q2)
+        print("d   = ", d)
+        print("res = ", res)
+
+    return res                              
+
 model=Siamese()
